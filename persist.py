1bcdb90a504e9d5d812a12764a2d667d45350579
Jacob Casper <jacob.thomas.casper@gmail.com>
2020-07-30 21:26:36 -0500

    Check and update work queue statements

--- persist.py
+++ persist.py
@@ -73,3 +73,41 @@ def rest_case(case):
             ),
         )
     curs.close()
+
+
+def get_case_work_logs(case_ids):
+    """
+    Looks up CASE_WORK_LOGs by case_id and yields them.
+    """
+
+    if type(case_ids) is not list:
+        # Handle a single case_id being requested
+        case_ids = [case_ids]
+
+    questions = ",".join(["?" for i in range(len(case_ids))])
+
+    conn = sqlite3.connect("cases.db")
+    conn.execute("pragma journal_mode=wal")
+    conn.row_factory = sqlite3.Row
+
+    curs = conn.cursor()
+    curs.execute(f"SELECT * FROM CASE_WORK_LOG WHERE ID IN ({questions})", case_ids)
+    for work_log in curs:
+        yield dict(work_log)
+
+
+def insert_case_work_logs(case_ids):
+    """
+    Inserts or marks CASE_WORK_LOGs so that they will be picked up and scraped.
+    """
+    if type(case_ids) is not list:
+        # Handle a single case_id being requested
+        case_ids = [case_ids]
+
+    conn = sqlite3.connect("cases.db", isolation_level=None)
+    conn.execute("pragma journal_mode=wal")
+    curs = conn.cursor()
+    curs.executemany(
+        "INSERT INTO CASE_WORK_LOG (ID, NEEDS_WORK) VALUES (?, 1) ON CONFLICT (ID) DO UPDATE SET NEEDS_WORK=1",
+        case_ids,
+    )
