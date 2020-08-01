a85f363778f5ddd8ccc5120402ec09e5c1e31775
Jacob Casper <jacob.thomas.casper@gmail.com>
2020-07-31 21:25:40 -0500

    Split into smaller chunks
    My tiny VPS only has 1GB of RAM so it needs to have smaller files to
    work than my desktop does. This is due to Firefox's Web Content cache
    never purging itself.

--- app/look-for-work.sh
+++ app/look-for-work.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sqlite3 cases.db 'select id from case_work_log where needs_work = 1' \
-    | split --filter='MOZ_HEADLESS=1 /opt/eviction-hearing-parser/venv/bin/python /opt/eviction-hearing-parser/app/scrape_worker.py' -
+    | split --lines=100 --filter='MOZ_HEADLESS=1 /opt/eviction-hearing-parser/venv/bin/python /opt/eviction-hearing-parser/app/scrape_worker.py' -
